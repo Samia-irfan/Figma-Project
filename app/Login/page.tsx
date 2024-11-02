423f50f7a0da9ec9afcdf9f0cee1daaf562ae04f
Samia Irfan <160426058+Samia-irfan@users.noreply.github.com>
2024-11-02 08:35:18 +0500

    Add files via upload

---
+++ app/Login/page.tsx
@@ -0,0 +1,11 @@
+export default function Login(){
+    return(
+        <div>
+            <h1>Login
+                <div>
+                    <button className="text-center text-white font-Montserrat w-110px h-55px r-6px bg-blue-700 p-1 rounded-md ">Login</button>
+                </div>
+            </h1>
+        </div>
+    )
+}
